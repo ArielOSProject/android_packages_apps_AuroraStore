cd3442e1d9ab5c34045789a471fe67115d0fc5c4
Sava Mikalacki <mikalackis@gmail.com>
2024-08-28 17:26:00 +0200

    Add LOCAL_OPTIONAL_USES_LIBRARIES

--- Android.mk
+++ Android.mk
@@ -5,6 +5,7 @@ LOCAL_MODULE := AuroraStore
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SYSTEM_EXT_MODULE := true
 LOCAL_MODULE_CLASS := APPS
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
 # 4.3.1
 LOCAL_SRC_FILES := AuroraStore.apk
 include $(BUILD_PREBUILT)
