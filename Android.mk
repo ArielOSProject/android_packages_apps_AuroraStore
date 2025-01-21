d4ab05eaf07b72ec2e09fcb80fc6a6eb7d825d62
Sava Mikalacki <mikalackis@gmail.com>
2025-01-21 10:04:12 +0100

    Bump to 4.6.4

--- Android.mk
+++ Android.mk
@@ -8,9 +8,9 @@ LOCAL_MODULE_CLASS := APPS
 ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 31),)
 LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
 endif
-# 4.6.2
+# 4.6.4
 LOCAL_SRC_FILES := AuroraStore.apk
-# this is needed because of the No APK Signature Scheme v2 signature in package error for version 4.6.2
+# this is needed because of the No APK Signature Scheme v2 signature in package error for version 4.6.4
 # which results in app not being installed.
 # Apparently, AOSP strips down the apk and also strips down the Signature Scheme v2 while stripping it
 # so we use this flag to maintain the scheme and allow the app to be properly installed.
