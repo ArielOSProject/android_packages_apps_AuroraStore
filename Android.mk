7a3ad373b8a63ca4a792e59d3acda1858c227110
Sava Mikalacki <sava.mikalacki@navicogroup.com>
2026-02-26 10:02:57 +0100

    Bump to 4.8.1-preload

--- Android.mk
+++ Android.mk
@@ -9,7 +9,7 @@ ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 31),)
 LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
 endif
 # 4.6.4
-LOCAL_SRC_FILES := AuroraStore.apk #4.7.5
+LOCAL_SRC_FILES := AuroraStore.apk #4.8.1
 # this is needed because of the No APK Signature Scheme v2 signature in package error for version 4.6.4
 # which results in app not being installed.
 # Apparently, AOSP strips down the apk and also strips down the Signature Scheme v2 while stripping it
